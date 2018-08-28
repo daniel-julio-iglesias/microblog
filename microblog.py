feb1feda2ddb5b4239e4653a64ac75fee9a9e37e
Daniel Julio Iglesias Hambálek <daniel.julio.iglesias@gmail.com>
2018-08-28 20:46:18 +0300

    The Flask Mega-Tutorial Part VII: Error Handling Setting PyCharm for Debug on Flask

--- microblog.py
+++ microblog.py
@@ -5,3 +5,7 @@ from app.models import User, Post
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True, threaded=False)
