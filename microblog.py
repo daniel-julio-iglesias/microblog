abe00e2e6e456bcd5a88a7a791315b078c3d71cb
Daniel Julio Iglesias Hambálek <daniel.julio.iglesias@gmail.com>
2018-08-28 21:40:09 +0300

    The Flask Mega-Tutorial Part VII: Error Handling Setting PyCharm for Debug on Flask

--- microblog.py
+++ microblog.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: ascii -*-
+
+
+#/usr/bin/env python3
+# coding: cp1252
+# Any of the following string literal forms work in latin-1.
+# Changing the encoding above to either ascii or utf-8 fails,
+# because the 0xc4 and 0xe8 in myStr1 are not valid in either.
+
+
 from app import app, db
 from app.models import User, Post
 
