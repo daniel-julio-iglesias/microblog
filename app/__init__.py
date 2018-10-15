e7266b6ccf413a4cf94203020da06aef6fbff6df
daniel-julio-iglesias <daniel.julio.iglesias@gmail.com>
2018-10-15 21:55:45 +0300

    [UPD] The Flask Mega-Tutorial Part XIII: I18n and L10n

--- app/__init__.py
+++ app/__init__.py
@@ -56,7 +56,8 @@ if not app.debug:
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    # return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'es'
 
 
 from app import routes, models, errors
